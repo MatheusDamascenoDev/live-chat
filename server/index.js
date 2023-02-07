c7ab49494a990762599598fcf67552aea5a41b10
Matheus Damasceno Jaques <matheusdjaques@gmail.com>
2023-02-06 23:00:51 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const _dirname = path.resolve();
 
 const io = new Server(server, {
   cors: {
-    origin: "https://livechatapp.herokuapp.com",
+    origin: "http://localhost:3001",
     methods: ["GET","POST"],
     allowedHeaders: ['*'],
     credentials: true
