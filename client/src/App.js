afe6427700cf0a1957760de4f4c05bf5845e17be
Matheus Damasceno Jaques <matheusdjaques@gmail.com>
2023-04-18 14:06:40 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 import { useState } from 'react';
 import Chat from './Chat';
 
-const socket = io.connect('http://localhost:3001');
+const socket = io.connect('https://live-chat-xcbi.onrender.com/');
 
 function App() {
   const [username, setUsername] = useState('');
